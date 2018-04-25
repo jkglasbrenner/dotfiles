e7e813dd2633b870b7de1c08131fbeed21c0d708
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-04-25 09:20:47 -0400

    Just store spacemacs in .emacs.d

--- emacs/dot-emacs.d/init.el
+++
@@ -1,4 +0,0 @@
-(package-initialize)
-
-(setq spacemacs-start-directory (file-name-as-directory (expand-file-name "spacemacs" user-emacs-directory)))
-(load-file (expand-file-name "init.el" spacemacs-start-directory))
