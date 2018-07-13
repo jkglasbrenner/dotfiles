b377a470ac99d60512c9e756b8cb404a87395a70
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-07-13 16:11:50 -0400

    Add private layer for polymode

---
+++ emacs/dot-emacs.d/private/+lang/polymode/funcs.el
@@ -0,0 +1,16 @@
+;;; funcs.el --- polymode layer functions file for Spacemacs
+;;
+;; Author: James K. Glasbrenner <jglasbr2@gmu.edu>
+;; URL: https://github.com/jkglasbrenner/dotfiles
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: Unlicense
+
+(defalias 'r-mode 'R-mode)
+
+(defun spacemacs//set-variables-for-polymode ()
+  "Set variables for polymode."
+  )
+
+;;; funcs.el ends here
---
+++ emacs/dot-emacs.d/private/+lang/polymode/packages.el
@@ -0,0 +1,38 @@
+;;; packages.el --- polymode layer packages file for Spacemacs.
+;;
+;; Author: James K. Glasbrenner <jglasbr2@gmu.edu>
+;; URL: https://github.com/jkglasbrenner/dotfiles
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: Unlicense
+
+;;; Commentary:
+
+;; Add layer for polymode to Spacemacs
+
+;;; Code:
+
+(setq polymode-packages
+      '(ess
+        (polymode (recipe :fetcher github
+                          :repo "vspinu/polymode"
+                          :branch "next"))))
+
+(defun polymode/init-polymode ()
+  (use-package polymode
+    :defer t
+    :mode (("\\.Snw$" . poly-noweb+r-mode)
+           ("\\.Rnw$" . poly-noweb+r-mode)
+           ("\\.Rmd$" . poly-markdown+r-mode)
+           ("\\.rapport$" . poly-rapport-mode)
+           ("\\.Rhtml$" . poly-html+r-mode)
+           ("\\.Rbrew$" . poly-brew+r-mode)
+           ("\\.Rcpp$" . poly-r+c++-mode)
+           ("\\.cppR$" . poly-c++r-mode)
+           ("\\.py\\'" . python-mode))
+    :config
+    (progn
+      (spacemacs//set-variables-for-polymode))))
+
+;;; packages.el ends here
