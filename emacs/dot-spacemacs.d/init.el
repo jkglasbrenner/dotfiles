78099cdd5be69efe977a0bdad58586735256e11b
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-07-13 16:12:37 -0400

    Define function for unfilling sentences

--- emacs/dot-spacemacs.d/init.el
+++ emacs/dot-spacemacs.d/init.el
@@ -512,6 +512,34 @@ This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
+  (defun ales/fill-paragraph (&optional P)
+    "When called with prefix argument call `fill-paragraph'.
+Otherwise split the current paragraph into one sentence per line.
+
+Source: http://www.cs.au.dk/~abizjak/emacs/2016/03/06/latex-fill-paragraph.html"
+    (interactive "P")
+    (if (not P)
+        (save-excursion 
+          (let ((fill-column 12345678)) ;; relies on dynamic binding
+            (fill-paragraph) ;; this will not work correctly if the paragraph is
+            ;; longer than 12345678 characters (in which case the
+            ;; file must be at least 12MB long. This is unlikely.)
+            (let ((end (save-excursion
+                         (forward-paragraph 1)
+                         (backward-sentence)
+                         (point-marker))))  ;; remember where to stop
+              (beginning-of-line)
+              (while (progn (forward-sentence)
+                            (<= (point) (marker-position end)))
+                (just-one-space) ;; leaves only one space, point is after it
+                (delete-char -1) ;; delete the space
+                (newline)        ;; and insert a newline
+                (LaTeX-indent-line) ;; I only use this in combination with late, so this makes sense
+                ))))
+      ;; otherwise do ordinary fill paragraph
+      (fill-paragraph P)))
+  (spacemacs/set-leader-keys "xQ" 'ales/fill-paragraph)
+  (spacemacs/set-leader-keys "xq" 'unfill-toggle)
   (setq
    TeX-engine 'luatex
    tab-width 4
